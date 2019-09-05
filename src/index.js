a2de8d8406850d40909a3b5f901a78b7479a871f
Tyler Biswell <tsbiswell@gmail.com>
2019-09-05 11:31:30 -0600

    add routing to app and home page

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
-import React, { useState, useEffect } from "react";
-import ReactDOM from "react-dom";
-import axios from "axios";
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import ReactDOM from 'react-dom';
+import axios from 'axios';
 
-import Charts from "./components/Charts";
-import Navbar from "./components/Navbar";
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import Charts from './components/Charts';
 
-import "./styles.scss";
+import './styles.scss';
 
 const App = () => {
   const [coinData, setCoinData] = useState([]);
@@ -13,18 +15,28 @@ const App = () => {
   useEffect(() => {
     axios
       .get(
-        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=true"
+        'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=true',
       )
       .then(res => setCoinData(res.data))
       .catch(err => console.log(err));
   }, []);
+
   return (
-    <div className="App">
+    <div className='App'>
       <Navbar />
-      <Charts coinData={coinData} />
+      <Route exact path='/' component={Home} />
+      <Route
+        path='/charts'
+        render={props => <Charts {...props} coinData={coinData} />}
+      />
     </div>
   );
 };
 
-const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
+const rootElement = document.getElementById('root');
+ReactDOM.render(
+  <Router>
+    <App />
+  </Router>,
+  rootElement,
+);
